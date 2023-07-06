efc3573025dd8c8ba4e03dfa64f1489c28bc7c44
cccctyl <cccctyl@qq.com>
2023-07-06 18:49:07 +0800

    modify： 修复空对象redis序列化失败问题

--- src/main/java/io/github/cctyl/config/RedisConfig.java
+++ src/main/java/io/github/cctyl/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package io.github.cctyl.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -16,12 +18,13 @@ public class RedisConfig {
 
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         //key 的序列化方式
         redisTemplate.setKeySerializer(new StringRedisSerializer());
 
         //值的序列化方式
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 
         //hash 类型的 key 和 value的序列化方式
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
