4b04a671eb0e714a4d82fa05b90ddb62d9900515
cccctyl <cccctyl@qq.com>
2023-08-30 09:12:55 +0800

    modify: 更新配置文件

--- cmd/start.sh
+++ cmd/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 java $@  -XX:+AlwaysPreTouch -Duser.timezone=GMT+08 \
-  -Xmx300m -Xms300m BiliRecommBehaviorCorrection-1.0-SNAPSHOT.jar
+-Xmx300m -Xms300m BiliRecommBehaviorCorrection-1.0-SNAPSHOT.jar
