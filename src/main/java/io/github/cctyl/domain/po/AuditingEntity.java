1967aed30a0b0487fbc88544f74eb936513ca911
cccctyl <cccctyl@qq.com>
2023-11-27 10:34:19 +0800

    modify: 两个字段不再返回给前端

--- src/main/java/io/github/cctyl/domain/po/AuditingEntity.java
+++ src/main/java/io/github/cctyl/domain/po/AuditingEntity.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.Version;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,10 +23,12 @@ public abstract class AuditingEntity implements Serializable {
 
     @TableField(value = "is_deleted",fill = FieldFill.INSERT)
     @TableLogic
+    @JsonIgnore
     private Integer isDeleted;
 
     @TableField(value = "version",fill = FieldFill.INSERT)
     @Version
+    @JsonIgnore
     private Integer version;
 
     @TableField(value = "last_modified_date",fill = FieldFill.INSERT_UPDATE)
