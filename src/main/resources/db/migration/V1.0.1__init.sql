0e8ac244f70219ca183db26b2a53d7e48b4d9031
cctyl <cccctyl@qq.com>
2025-02-02 16:39:54 +0800

    modify: 恢复flyway版本控制

--- src/main/resources/db/migration/V1.0.1__init.sql
+++ src/main/resources/db/migration/V1.0.1__init.sql
@@ -1,137 +1,98 @@
--- dict definition
-
-CREATE TABLE dict
+create table config
 (
-    id                 char(30)     NOT NULL,
-    value              VARCHAR(255) NOT NULL,
-    access_type        varchar(50)  null,
-    dict_type          varchar(50)  null,
-    outer_id           varchar(60)  NULL,
-    desc               varchar(50)  NULL,
-
-    created_date       DATE         null,
-    last_modified_date DATE         null,
-    is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_dict PRIMARY KEY (id)
-);
-
-
--- owner definition
-
-CREATE TABLE owner
-(
-    id                 char(30) PRIMARY KEY,
-    mid                VARCHAR(100) unique not null,
-    name               VARCHAR(100)        not null,
-    face               VARCHAR(350)        NULL,
-    created_date       DATE                null,
-    last_modified_date DATE                null,
+    id                 char(30)    not null
+        constraint pk_config
+            primary key,
+    name               varchar(50) not null,
+    value              varchar(200),
+    expire_second      int        default -1,
+    created_date       DATE,
+    last_modified_date DATE,
     is_deleted         tinyint(1) default 0,
     version            int        default 1
-
 );
 
-
--- stat definition
-
-CREATE TABLE stat
+create table cookie_header_data
 (
-    id                 char(30) NOT NULL,
-    aid                INT      NOT NULL,
-    view               INT      NULL,
-    danmaku            INT      NULL,
-    reply              INT      NULL,
-    favorite           INT      NULL,
-    coin               INT      NULL,
-    share              INT      NULL,
-    now_rank           INT      NULL,
-    his_rank           INT      NULL,
-    `like`             INT      NULL,
-    dislike            INT      NULL,
-    vt                 INT      NULL,
-    vv                 INT      NULL,
-    video_id           char(30) NULL,
-    created_date       DATE     null,
-    last_modified_date DATE     null,
+    id         CHAR    not null
+        constraint pk_cookie_header_data
+            primary key,
+    url        VARCHAR,
+    ckey       VARCHAR not null,
+    cvalue     VARCHAR not null,
+    classify   VARCHAR,
+    media_type VARCHAR
+);
+
+create table dict
+(
+    id                 char(30)     not null
+        constraint pk_dict
+            primary key,
+    value              VARCHAR(255) not null,
+    access_type        varchar(50),
+    dict_type          varchar(50),
+    outer_id           varchar(60),
+    created_date       DATE,
+    last_modified_date DATE,
     is_deleted         tinyint(1) default 0,
     version            int        default 1,
-    CONSTRAINT pk_stat PRIMARY KEY (id)
+    desc               varchar(50)
 );
 
-
--- video_detail definition
-
-CREATE TABLE video_detail
+create table owner
 (
-    id                   CHAR(30)     NOT NULL,
-    aid                  INT          not null unique,
-    videos               INT          NULL,
-    tid                  INT          NULL,
-    tname                VARCHAR(255) NULL,
-    copyright            INT          NULL,
-    pic                  VARCHAR(350) NULL,
-    title                VARCHAR(255) NULL,
-    pubdate              INT          NULL,
-    ctime                INT          NULL,
-    `desc`               VARCHAR(255) NULL,
-    state                INT          NULL,
-    duration             INT          NULL,
-    mission_id           INT          NULL,
-    dynamic              VARCHAR(255) NULL,
-    cid                  INT          NULL,
-    season_id            INT          NULL,
-    short_link_v2        VARCHAR(255) NULL,
-    first_frame          VARCHAR(255) NULL,
-    pub_location         VARCHAR(255) NULL,
-    bvid                 VARCHAR(255) not null unique,
-    season_type          INT          NULL,
-    is_ogv               TINYINT(1)   NULL,
-    owner_id             CHAR(30)     NULL,
-    handle               TINYINT(1) default 0,
-    black_reason         text         null,
-    thumb_up_reason      text         null,
-    teenage_mode         INT          NULL,
-    is_chargeable_season TINYINT(1)   NULL,
-    is_story             TINYINT(1)   NULL,
-    is_upower_exclusive  TINYINT(1)   NULL,
-    is_upower_play       TINYINT(1)   NULL,
-    no_cache             TINYINT(1)   NULL,
-    is_season_display    TINYINT(1)   NULL,
-    like_icon            VARCHAR(255) NULL,
-    need_jump_bv         TINYINT(1)   NULL,
-    enable_vt            INT          NULL,
-    disable_show_up_info TINYINT(1)   NULL,
-    up_from_v2           INT          NULL,
-    rcmd_reason          VARCHAR(255) NULL,
-    score                INT          NULL,
-    handle_type          varchar(50)  null,
-    created_date         DATE         null,
-    last_modified_date   DATE         null,
-    is_deleted           tinyint(1) default 0,
-    version              int        default 1,
-
-    CONSTRAINT pk_video_detail PRIMARY KEY (id)
+    id                 char(30)
+        primary key,
+    mid                VARCHAR(100) not null
+        unique,
+    name               VARCHAR(100) not null,
+    face               VARCHAR(350),
+    created_date       DATE,
+    last_modified_date DATE
 );
 
-
--- white_list_rule definition
-
-CREATE TABLE white_list_rule
+create table prepare_video
 (
-    id                 char(30)     NOT NULL,
-    info               varchar(100) null,
-    created_date       DATE         null,
-    last_modified_date DATE         null,
+    id                 char(30)    not null
+        constraint pk_prepare_video
+            primary key,
+    video_id           char(30)    not null,
+    handle_type        varchar(30) not null,
+    created_date       DATE,
+    last_modified_date DATE,
     is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_white_list_rule PRIMARY KEY (id)
+    version            int        default 1
 );
 
--- tag
-CREATE TABLE tag
+create table stat
 (
-    id                 char(30)     NOT NULL,
+    id                 char(30) not null
+        constraint pk_stat
+            primary key,
+    aid                INT      not null,
+    view               INT,
+    danmaku            INT,
+    reply              INT,
+    favorite           INT,
+    coin               INT,
+    share              INT,
+    now_rank           INT,
+    his_rank           INT,
+    like               INT,
+    dislike            INT,
+    vt                 INT,
+    vv                 INT,
+    video_id           char(30),
+    created_date       DATE,
+    last_modified_date DATE
+);
+
+create table tag
+(
+    id                 char(30)     not null
+        constraint pk_tag
+            primary key,
     tag_id             int          not null,
     tag_name           varchar(255) not null,
     cover              VARCHAR(350),
@@ -158,155 +119,155 @@ CREATE TABLE tag
     archive_count      VARCHAR(50),
     featured_count     INTEGER,
     jump_url           VARCHAR(350),
-    created_date       DATE         null,
-    last_modified_date DATE         null,
-    is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_tag PRIMARY KEY (id),
-    UNIQUE (tag_id,
-            tag_name)
+    created_date       DATE,
+    last_modified_date DATE,
+    unique (tag_id, tag_name)
 );
 
--- video与tag 关联表
-CREATE TABLE video_tag
+create table task
 (
-
-    id                 char(30) NOT NULL,
-    tag_id             char(30) not null,
-    video_id           char(30) not null,
-    created_date       DATE     null,
-    last_modified_date DATE     null,
-    is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_video_tag PRIMARY KEY (id)
+    id                 char(30) not null
+        constraint pk_task
+            primary key,
+    last_run_time      DATE,
+    current_run_status TEXT,
+    total_run_count    int,
+    last_run_duration  int,
+    task_name          TEXT,
+    scheduled_hour     int,
+    is_enabled         int,
+    class_method_name  text,
+    description        text,
+    img                text
 );
 
-CREATE TABLE cookie_header_data
+create table video_detail
 (
-    id                 char(30)     NOT NULL,
-    url                VARCHAR(350) NULL,
-    ckey               varchar(50)  NOT null,
-    cvalue             varchar(200) NOT null,
-    classify           varchar(50)  null,
-    media_type         varchar(50)  null,
-    created_date       DATE         null,
-    last_modified_date DATE         null,
-    is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_cookie_header_data PRIMARY KEY (id)
+    id                   CHAR(30)     not null
+        constraint pk_video_detail
+            primary key,
+    aid                  INT          not null
+        unique,
+    videos               INT,
+    tid                  INT,
+    tname                VARCHAR(255),
+    copyright            INT,
+    pic                  VARCHAR(350),
+    title                VARCHAR(255),
+    pubdate              INT,
+    ctime                INT,
+    desc                 VARCHAR(255),
+    state                INT,
+    duration             INT,
+    mission_id           INT,
+    dynamic              VARCHAR(255),
+    cid                  INT,
+    season_id            INT,
+    short_link_v2        VARCHAR(255),
+    first_frame          VARCHAR(255),
+    pub_location         VARCHAR(255),
+    bvid                 VARCHAR(255) not null
+        unique,
+    season_type          INT,
+    is_ogv               TINYINT(1),
+    owner_id             CHAR(30),
+    handle               TINYINT(1) default 0,
+    black_reason         text,
+    thumb_up_reason      text,
+    teenage_mode         INT,
+    is_chargeable_season TINYINT(1),
+    is_story             TINYINT(1),
+    is_upower_exclusive  TINYINT(1),
+    is_upower_play       TINYINT(1),
+    no_cache             TINYINT(1),
+    is_season_display    TINYINT(1),
+    like_icon            VARCHAR(255),
+    need_jump_bv         TINYINT(1),
+    enable_vt            INT,
+    disable_show_up_info TINYINT(1),
+    up_from_v2           INT,
+    rcmd_reason          VARCHAR(255),
+    score                INT,
+    handle_type          varchar(50),
+    created_date         DATE,
+    last_modified_date   DATE,
+    is_deleted           tinyint(1) default 0,
+    version              int        default 1
 );
 
+create index idx_aid
+    on video_detail (aid);
 
-CREATE TABLE config
-(
-    id                 char(30)     NOT NULL,
-    name               varchar(50)  NOT null,
-    value              varchar(200) null,
-    expire_second      int        default -1,
-    created_date       DATE         null,
-    last_modified_date DATE         null,
-    is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_config PRIMARY KEY (id)
-);
+create index idx_video_detail_handle_type
+    on video_detail (handle_type);
 
--- 视频 与 相关视频 的关联表
 create table video_relate
 (
+    id               char(30) not null
+        constraint pk_video_tag
+            primary key,
+    master_video_id  char(30) not null,
+    related_video_id char(30) not null
+);
 
-    id                 char(30) not null,
-    master_video_id    char(30) not null,
-    related_video_id   char(30) not null,
-    created_date       DATE     null,
-    last_modified_date DATE     null,
+create table video_reply
+(
+    id                 char(30)      not null
+        constraint pk_video_reply
+            primary key,
+    created_date       DATE,
+    last_modified_date DATE,
+    video_id           char(30)      not null,
+    rpid               bigint        not null,
+    oid                bigint        not null,
+    mid                varchar(200)  not null,
+    root               bigint        not null,
+    parent             bigint        not null,
+    dialog             bigint        not null,
+    ctime              int           not null,
+    current_level      int           not null,
+    vip_type           int           not null,
+    message            varchar(2000) not null,
+    sex                varchar(20)
+);
+
+create table video_tag
+(
+    id                 char(30) not null
+        constraint pk_video_tag
+            primary key,
+    tag_id             char(30) not null,
+    video_id           char(30) not null,
+    created_date       DATE,
+    last_modified_date DATE,
     is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    constraint pk_video_tag primary key (id)
+    version            int        default 1
 );
 
-
-CREATE TABLE video_reply
+create table white_list_rule
 (
-    id char(30) NOT NULL,
-    created_date DATE null,
-    last_modified_date DATE null,
---    视频id
-    video_id char(30) not null ,
---	评论id
-    rpid bigint not null,
---    评论区对象 id
-    oid bigint not null,
---	发送者 mid
-    mid varchar(200) not null,
---	根评论 rpid
---	若为一级评论则为 0
---	大于一级评论则为根评论 id
-    root bigint not null,
---	回复父评论 rpid
---	若为一级评论则为 0
---	若为二级评论则为根评论 rpid
---	大于二级评论为上一级评 论 rpid
-    parent bigint not null,
---	回复对方 rpid
---	若为一级评论则为 0
---	若为二级评论则为该评论 rpid
---	大于二级评论为上一级评论 rpid
-    dialog bigint not null,
---	评论发送时间
-    ctime int not null,
--- 评论者等级
-    current_level int not null,
--- 评论者vip状态
-    vip_type int not null,
---    评论信息
-    message varchar(2000) not null,
-
-
--- 性别
-    sex varchar(20),
-
-    CONSTRAINT pk_video_reply PRIMARY KEY (id)
-);
-CREATE TABLE prepare_video
-(
-    id                 char(30)    NOT NULL,
-    video_id           char(30)    not null,
-    handle_type        varchar(30) not null,
-    created_date       DATE        null,
-    last_modified_date DATE        null,
+    id                 char(30) not null
+        constraint pk_white_list_rule
+            primary key,
+    info               varchar(100),
+    created_date       DATE,
+    last_modified_date DATE,
     is_deleted         tinyint(1) default 0,
-    version            int        default 1,
-    CONSTRAINT pk_prepare_video PRIMARY KEY (id)
-);
-
-drop table  if exists task;
-CREATE TABLE task
-(
-    id                 char(30) NOT NULL,
-    last_run_time      DATE, -- 上次运行时间
-    current_run_status int,  -- 当前运行状态
-    total_run_count    int,  -- 总运行次数
-    last_run_duration  int,  -- 上次运行花费了多久
-    task_name          TEXT, -- 任务名
-    scheduled_hour     int,  -- 定时执行的时间，整点
-    is_enabled         int,  -- 是否开启定时任务
-    class_method_name  text, --该任务对应的方法路径
-    description        text,-- 任务描述
-    img                text,--任务图片
-    CONSTRAINT pk_task PRIMARY KEY (id)
+    version            int        default 1
 );
 
 
+insert into task (id, last_run_time, current_run_status, total_run_count, last_run_duration, task_name, scheduled_hour, is_enabled, class_method_name, description, img)
+values  ('1', '1738465200025', 'STOPPED', 0, 0, '关键词搜索任务', 11, 1, 'io.github.cctyl.service.impl.BiliService.doSearchTask', '根据设定的关键词进行搜索，搜索的数据加入待处理列表', 'fas fa-search'),
+        ('2', '1738328400042', 'STOPPED', 0, 0, '热门排行榜任务', 21, 1, 'io.github.cctyl.service.impl.BiliService.doHotRankTask', '抓取热门排行榜的数据，加入待处理列表', 'fas fa-chart-line'),
+        ('3', '1738464340143', 'STOPPED', 0, 0, '首页推荐任务', 6, 1, 'io.github.cctyl.service.impl.BiliService.doHomeRecommendTask', '连续抓取首页排行榜的数据，加入待处理列表', 'fas fa-home'),
+        ('4', '1738311862978', 'STOPPED', 0, 0, '处理视频-就按你说的做', 12, 0, 'io.github.cctyl.service.impl.BiliService.doDefaultProcessVideo', '按机器判断处理所有未人工审核的视频，程序初始判断的是白名单就按白名单处理，是黑名单就按黑名单处理', 'fas fa-robot'),
+        ('5', '1738472400032', 'STOPPED', 0, 0, '处理视频-触发三次处理', 13, 1, 'io.github.cctyl.service.impl.BiliService.doThirdProcess', '进行三次处理，按照二次处理的结果对视频点赞或点踩，单次最大处理80条数据', 'fas fa-play-circle'),
+        ('1883876188535939073', '1737986058762', 'STOPPED', 0, 0, null, -1, 0, 'io.github.cctyl.controller.ReplyController.saveVideoReplay', null, null),
+        ('1883876237844176897', '1737986422145', 'STOPPED', 0, 0, null, -1, 0, 'io.github.cctyl.controller.BlackRuleController.dislikeByTid', null, null),
+        ('1883876352046686210', '1737986097726', 'STOPPED', 0, 0, null, -1, 0, 'io.github.cctyl.controller.BlackRuleController.dislikeByUserId', null, null),
+        ('1883876385634672642', '1737986105747', 'STOPPED', 0, 0, null, -1, 0, 'io.github.cctyl.controller.WhiteRuleController.thumbUpUserAllVideo', null, null),
+        ('1883877101111631873', '1737986284913', 'STOPPED', 0, 0, null, -1, 0, 'io.github.cctyl.controller.WhiteRuleController.addTrain', null, null);
 
-CREATE TABLE task (
-       id  char(30)    NOT NULL,
-       last_run_time DATE,  -- 上次运行时间
-       current_run_status int,  -- 当前运行状态
-       has_scheduled_task int,  -- 待会有没有定时任务要执行（注意系统级别的定时任务开关有没有打开）
-       total_run_count int,  -- 总运行次数
-       last_run_duration int,  -- 上次运行花费了多久
-       task_name TEXT,  -- 任务名
-       scheduled_run_time int,  -- 定时执行的时间，整点
-       is_scheduled_task_enabled int,  -- 是否开启定时任务
-       CONSTRAINT pk_task PRIMARY KEY (id)
-);
+
+INSERT INTO cookie_header_data (id, url, ckey, cvalue, classify, media_type) VALUES ('658cf182905410d38abf75808ac01a2f', null, 'User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36', 'REQUEST_HEADER', 'GENERAL');
