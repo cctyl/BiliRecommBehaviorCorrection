afce6f77ede3ab2f07bba28e03e217e518c2e4d5
cctyl <cccctyl@qq.com>
2026-04-11 22:25:08 +0800

    feat: 任务列表

--- rust/src/handler/task_handler.rs
+++ rust/src/handler/task_handler.rs
@@ -1,15 +1,21 @@
-use crate::app::response::{OkRespExt, RR};
+use crate::app::config::CC;
+use crate::app::error::HttpError;
+use crate::app::response::{FailRespExt, OkRespExt, RR};
 use crate::domain::dtos::{PageDTO, SearchHandleVideoRequest, SecondHandleDto, VideoVo};
+use crate::domain::task::Task;
 use crate::service::{task_service, video_detail_service};
 use axum::extract::Query;
-use axum::routing::{get, post};
+use axum::routing::{get, post, put};
 use axum::{Json, Router, debug_handler};
+use rbs::value;
 
 pub fn create_router() -> Router {
     Router::new()
         .route("/second-process", axum::routing::post(second_handler))
         .route("/batch-second-process", post(batch_second_handle))
         .route("/already-handle", get(search_handle_video))
+        .route("/task-list", get(task_list))
+        .route("/", put(update_task))
 }
 
 /// 二次处理视频
@@ -41,4 +47,33 @@ pub async fn search_handle_video(
     .await?;
 
     RR::success(get_handle_video)
+}
+
+
+/// 任务列表
+#[debug_handler]
+pub async fn task_list()->RR<Vec<Task>>{
+
+    let list = Task::select_by_map(&CC.rb, value!{}).await?;
+
+    RR::success(list)
+}
+
+/// 修改任务信息
+#[debug_handler]
+pub async fn update_task(Json(t):Json<Task>)->RR<()>{
+
+
+    let update_by_id: rbatis::rbdc::db::ExecResult = Task::update_by_id(&CC.rb, &t).await?;
+
+
+    if update_by_id.rows_affected>0{
+        RR::success(())
+    }else {
+        
+        RR::fail(HttpError::BadRequest("更新失败！".to_string()))
+    }
+
+
+
 }
